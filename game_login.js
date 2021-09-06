8ae65ae6cdbd08b25b7ad7d7266e818570b48b83
VedTheBest2011 <82335213+VedTheBest2011@users.noreply.github.com>
2021-09-06 09:27:25 +0400

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -9,6 +9,21 @@ function addUser()
     window.location.replace("game_page.html");   
 }
 
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+
+player1_score = 0;
+player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name + " :";
+document.getElementById("player2_name").innerHTML = player2_name + " :";
+
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+
 function send() {
     number1 = document.getElementById("number_1").value;
     number2 = document.getElementById("number_2").value;
